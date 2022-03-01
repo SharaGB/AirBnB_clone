30e52d788d6ae41d1885931edd772c5073d8c20d
DaboRestrepo <daborestrepo@gmail.com>
2022-03-01 17:45:08 -0500

    update the branch

--- models/base_model.py
+++ models/base_model.py
@@ -2,42 +2,29 @@
 """
 Module to write a class BaseModel.
 """
-from uuid import uuid4          
+from uuid import uuid4
 import models
-import datetime
-date = '%Y-%m-%dT%H:%M:%S.%f'
+from datetime import datetime
+""" date = '%Y-%m-%dT%H:%M:%S.%f' """
 
 
 class BaseModel:
     """
     Defines all common attributes/methods for other classes.
     """
-
-    """ def __init__(self, *args, **kwargs):
-        
+    def __init__(self, *args, **kwargs):
+        self.id = str(uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+        """ models.storage.new(self) """
         if kwargs is not None:
             for key, value in kwargs.items():
                 if key != '__class__':
                     if key == 'created_at' or key == 'updated_at':
                         # Establecemos el valor del atributo especificado
                         # del objeto especificado.
-                        setattr(self, key, datetime.strptime(value, date))
+                        setattr(self, key, datetime.isoformat(value))
                     setattr(self, key, value)
-        else:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = self.created_at
-            models.storage.new(self) """
-
-    uniq_id = str(uuid4())
-    create_time = datetime.datetime.now().isoformat()
-    update_time = create_time
-
-    def __init__(self, id=uniq_id, create_at=create_time, update_at=update_time):
-        """"""
-        self.id = id
-        self.create_at = create_at
-        self.update_at = update_at
 
     def __str__(self):
         """ Return string representation. """
@@ -46,8 +33,8 @@ class BaseModel:
 
     def save(self):
         """ Updates attribute updated_at with the current datetime. """
-        self.updated_at = datetime.datetime.now().isoformat()
-        """ models.storage.save() """
+        self.updated_at = datetime.now().isoformat()
+        models.storage.save()
 
     def to_dict(self):
         """
