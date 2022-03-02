b8e33adb3001f0aeef7cc82a10da44565c602723
SharaGB <shara.garcia.betancur@gmail.com>
2022-03-02 15:45:32 -0500

    Add console

--- console.py
+++ console.py
@@ -10,8 +10,7 @@ class HBNBCommand(cmd.Cmd):
     prompt = '(hbnb) '
 
     def do_quit(self, line):
-        """ Quit command to exit the program.
-		"""
+        """ Quit command to exit the program. """
         return True
 
     def do_EOF(self, line):
