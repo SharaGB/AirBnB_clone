a60ee2bbc2f4184a1fe765bfee09be3945e3e045
DaboRestrepo <daborestrepo@gmail.com>
2022-03-01 19:15:19 -0500

    5 point

---
+++ models/engine/file_storage.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""
+
+"""
+
+
+import json
+
+
+class FileStorage:
+    """"""
+    __file_path = "file.json"
+    __objects = {}
+
+    def all(self):
+        """"""
+        return self.__objects
+
+    def new(self, obj):
+        """"""
+        key = obj.__class__.__name__ + '.id'
+        self.__objects[key] = obj
+
+    def save(self):
+        """"""
+        with open(self.__file_path, 'w') as file:
+            json.dump(self.__objects, file)
+
+    def reload(self):
+        """"""
+        try:
+            with open(self.__file_path, 'r') as file:
+                self.__objects = json.load(file)
+        except Exception:
+            pass
