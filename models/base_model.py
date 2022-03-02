a4dd9ea98a24c33dbe4b966167f30b33294b6e32
SharaGB <shara.garcia.betancur@gmail.com>
2022-03-01 21:16:27 -0500

    Add documentation

---
+++ models/base_model.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python3
+"""
+Module to write a class BaseModel.
+"""
+from uuid import uuid4
+import models
+from datetime import datetime
+from models import storage
+date = '%Y-%m-%dT%H:%M:%S.%f'
+
+
+class BaseModel:
+    """
+    Defines all common attributes/methods for other classes.
+    """
+
+    def __init__(self, *args, **kwargs):
+        """ Initializes the data. """
+        if kwargs is not None:
+            for key, value in kwargs.items():
+                if key != '__class__':
+                    if key == 'created_at' or key == 'updated_at':
+                        # Establecemos el valor del atributo especificado
+                        # del objeto especificado.
+                        setattr(self, key, datetime.strptime(value, date))
+                    setattr(self, key, value)
+        self.id = str(uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+        storage.new(kwargs)
+
+    def __str__(self):
+        """ Return string representation. """
+        return "[{:s}] ({:s}) {}".format(self.__class__.__name__, self.id,
+                                         self.__dict__)
+
+    def save(self):
+        """ Updates attribute updated_at with the current datetime. """
+        self.updated_at = datetime.now()
+        storage.save()
+
+    def to_dict(self):
+        """
+        Returns a dictionary containing all keys/values of the instance.
+        """
+        new_dict = self.__dict__.copy()
+        for key, value in new_dict.items():
+            if key == 'created_at' or key == 'updated_at':
+                new_dict[key] = datetime.isoformat(value)
+            else:
+                new_dict[key] = value
+        new_dict['__class__'] = self.__class__.__name__
+        return new_dict
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -2,7 +2,6 @@
 """
 Module to write a class FileStorage
 """
-from models.base_model import BaseModel
 import json
 
 
@@ -21,16 +20,15 @@ class FileStorage:
 
     def new(self, obj):
         """ Sets in __objects the obj with key <obj class name>.id """
-        key = obj.__class__.__name__ + '.' + obj.id
+        key = obj.__class__.__name__ + '.id'
         # In dictionaries, if the key does not exist, it creates it
         # and if it does exist, it replaces it.
         self.__objects[key] = obj
 
     def save(self):
         """ Serializes __objects to the JSON file (path: __file_path) """
-        dict = {key: value.to_dict() for key, value in self.__objects.items()}
         with open(self.__file_path, 'w') as file:
-            json.dump(dict, file)
+            json.dump(self.__objects, file)
 
     def reload(self):
         """ Seserializes the JSON file to __objects
