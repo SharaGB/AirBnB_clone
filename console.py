ee6a853627591483297f1a6146d6a07a9e6490c9
SharaGB <shara.garcia.betancur@gmail.com>
2022-03-03 11:39:41 -0500

    Update do_all function

--- console.py
+++ console.py
@@ -29,6 +29,7 @@ class HBNBCommand(cmd.Cmd):
         """ Create a new instance of BaseModel. """
         if not line:
             print("** class name missing **")
+            return False
         elif line != 'BaseModel':
             print("** class doesn't exist **")
         else:
@@ -41,6 +42,7 @@ class HBNBCommand(cmd.Cmd):
         splitline = split(line)
         if not splitline:
             print("** class name missing **")
+            return False
         elif splitline[0] != 'BaseModel':
             print("** class doesn't exist **")
         elif len(splitline) < 2:
@@ -57,6 +59,7 @@ class HBNBCommand(cmd.Cmd):
         splitline = split(line)
         if not splitline:
             print("** class name missing **")
+            return False
         elif splitline[0] != 'BaseModel':
             print("** class doesn't exist **")
         elif len(splitline) < 2:
@@ -68,6 +71,59 @@ class HBNBCommand(cmd.Cmd):
             else:
                 del models.storage.all()[new_instance]
                 models.storage.save()
+
+    def do_all(self, line):
+        """ Print a representation of all instance based
+        or not in the class name. """
+        # list = []
+        # list2 = []
+        # if not line or line == 'BaseModel':
+        #     for key, value in models.storage.all().items():
+        #         list.append(value.__str__())
+        #     for item in list:
+        #         list2.append(str(item))
+        #     print(list2)
+        # else:
+        #     print("** class doesn't exist **")
+
+        str_list = []
+        if not line:
+            for new_instance in models.storage.all().values():
+                str_list.append(str(new_instance))
+        else:
+            if line != 'BaseModel':
+                print("** class doesn't exist **")
+                return False
+            for key, value in models.storage.all().items():
+                left = key.split('.')[0]
+                if left == line:
+                    str_list.append(str(value))
+        print(str_list)
+
+        def do_update(self, line):
+            """ Updates an instance based on the class name and id
+                    by adding or updating attribute. """
+        splitline = split(line)
+        if not splitline:
+            print("** class name missing **")
+            return False
+        elif splitline[0] != 'BaseModel':
+            print("acá")
+            print("** class doesn't exist **")
+        elif len(splitline) < 2:
+            print("** instance id missing **")
+        elif len(splitline) < 3:
+            print("** attribute name missing **")
+        elif len(splitline) < 4:
+            print("** value missing **")
+        else:
+            new_instance = splitline[0] + '.' + splitline[1]
+            if new_instance not in models.storage.all():
+                print("** no instance found **")
+            else:
+                entry = {splitline[2]: splitline[3]}
+                models.storage.all().update(entry)  # [new_instance]
+                # models.storage.save()
 
 
 if __name__ == '__main__':
