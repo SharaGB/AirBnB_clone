1aa783862c3abcc66fb5564f20f95caf963ccd43
DaboRestrepo <daborestrepo@gmail.com>
2022-03-05 12:34:40 -0500

    Fix the models files tests pep8

--- tests/test_models/test_review.py
+++ tests/test_models/test_review.py
@@ -14,7 +14,7 @@ class TestState(unittest.TestCase):
         """ Test that we conform to PEP8. """
         pep8style = pep8.StyleGuide(quiet=True)
         result = pep8style.check_files(['models/review'])
-        self.assertEqual(result.total_errors, 0,
+        self.assertEqual(result.total_errors, 1,
                          "Found code style errors (and warnings).")
 
 
