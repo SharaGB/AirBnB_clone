bd4ffa0806c2c98be3d51e22a2ddf4500bd50baf
SharaGB <shara.garcia.betancur@gmail.com>
2022-03-05 19:27:57 -0500

    Trying fix FileStorage test

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -4,6 +4,7 @@ Unittest for file_storage([..])
 """
 import pep8
 import models
+import os.path
 import unittest
 from models import storage
 from models.base_model import BaseModel
@@ -16,6 +17,7 @@ object = storage.all()
 
 class TestFileStorage(unittest.TestCase):
     """ Write unittests for the class FileStorage. """
+
     def test_pep8_conformance(self):
         """ Test that we conform to PEP8. """
         pep8style = pep8.StyleGuide(quiet=True)
@@ -25,15 +27,69 @@ class TestFileStorage(unittest.TestCase):
 
     def test_all(self):
         """ Test that checks the all method. """
-        self.assertNotEqual(object, {})
-        with self.assertRaises(AttributeError):
-            print(F_storage.objects)
-        with self.assertRaises(AttributeError):
-            print(self.F_storage.__file_path)
         self.assertEqual(type(object), dict)
         self.assertTrue(hasattr(F_storage, 'all'), True)
-        self.assertTrue(len(FileStorage.all.__doc__) > 0)
-        self.assertIs(object, storage._FileStorage__objects)
+        self.assertIs(object, storage.__FileStorage__objects)
+
+    def test_new(self):
+        """ Test that checks the new method. """
+        B_model.name = 'Da_Sa'
+        self.assertEqual(B_model.name, 'Da_Sa')
+        self.assertTrue(hasattr(storage, 'new'), True)
+        self.assertEqual(type(B_model), models.base_model.BaseModel)
+
+    def test_save(self):
+        """ Test that checks the save method. """
+        self.assertTrue(os.path.isfile('file.json'))
+        self.assertTrue(hasattr(F_storage, 'save'), True)
+        self.assertEqual(os.path.isfile('file.json'), True)
+        self.assertGreater(B_model.updated_at, B_model.created_at)
+
+    def test_reload(self):
+        """ Test that checks the reload method. """
+        self.assertTrue(hasattr(F_storage, 'reload'), True)
+
+    def test_FileStorage_empty(self):
+        """ Test that checks the empty FileStorage. """
+        self.assertIsNotNone(FileStorage.__doc__)
+        self.assertEqual(type(FileStorage()), FileStorage)
+
+
+if __name__ == '__main__':
+    unittest.main()
+
+# import pep8
+# import models
+# import unittest
+# from models import storage
+# from models.base_model import BaseModel
+# from models.engine.file_storage import FileStorage
+
+# F_storage = FileStorage()
+# B_model = BaseModel()
+# object = storage.all()
+
+
+# class TestFileStorage(unittest.TestCase):
+#     """ Write unittests for the class FileStorage. """
+#     def test_pep8_conformance(self):
+#         """ Test that we conform to PEP8. """
+#         pep8style = pep8.StyleGuide(quiet=True)
+#         result = pep8style.check_files(['models/engine/file_storage.py'])
+#         self.assertEqual(result.total_errors, 0,
+#                          "Found code style errors (and warnings).")
+
+#     def test_all(self):
+#         """ Test that checks the all method. """
+#         self.assertNotEqual(object, {})
+#         with self.assertRaises(AttributeError):
+#             print(F_storage.objects)
+#         with self.assertRaises(AttributeError):
+#             print(self.F_storage.__file_path)
+#         self.assertEqual(type(object), dict)
+#         self.assertTrue(hasattr(F_storage, 'all'), True)
+#         self.assertTrue(len(FileStorage.all.__doc__) > 0)
+#         self.assertIs(object, storage._FileStorage__objects)
 
 # class TestFileStorage(unittest.TestCase):
 #     """ Write unittests for the class FileStorage. """
