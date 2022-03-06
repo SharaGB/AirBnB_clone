970679bcd645f881c5a2a88cf8947e22922989c1
DaboRestrepo <daborestrepo@gmail.com>
2022-03-05 19:14:35 -0500

    Tests

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -25,11 +25,15 @@ class TestFileStorage(unittest.TestCase):
 
     def test_all(self):
         """ Test that checks the all method. """
-        self.assertEqual(object, {})
+        # self.assertEqual(object, {})
+        with self.assertRaises(AttributeError):
+            print(F_storage.objects)
+        with self.assertRaises(AttributeError):
+            print(self.F_storage.__file_path)
         self.assertEqual(type(object), dict)
         self.assertTrue(hasattr(F_storage, 'all'), True)
-        self.assertTrue(len(FileStorage.all.doc) > 0)
-        self.assertIs(object, storage.FileStorageobjects)
+        self.assertTrue(len(FileStorage.all.__doc__) > 0)
+        self.assertIs(object, storage._FileStorage__objects)
 
 # class TestFileStorage(unittest.TestCase):
 #     """ Write unittests for the class FileStorage. """
@@ -52,10 +56,6 @@ class TestFileStorage(unittest.TestCase):
     #     """ Test that checks the all method. """
     #     F_storage = FileStorage()
     #     object_1 = storage.all()
-    #     # with self.assertRaises(AttributeError):
-    #     #     print(F_storage.objects)
-    #     # with self.assertRaises(AttributeError):
-    #     #     print(self.F_storage.__file_path)
     #     self.assertEqual(type(object_1), dict)
     #     # self.assertEqual(type(F_storage.all()), dict)
     #     # self.assertTrue(hasattr(F_storage, 'all'), True)
